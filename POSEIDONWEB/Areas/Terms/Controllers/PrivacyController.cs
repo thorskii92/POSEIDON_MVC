99e665c1f288d3ac52395c0b36b6cb48a3c9f16b
thorskii92 <nestorigna92@gmail.com>
2023-08-16 00:19:28 +0800

    Fixed UI of Login

---
+++ POSEIDONWEB/Areas/Terms/Controllers/PrivacyController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using POSEIDON.DataAccess.Repository.IRepository;
+using POSEIDONWEB.Areas.Consumer.Controllers;
+
+namespace POSEIDONWEB.Areas.Terms.Controllers
+{
+    [Area("Terms")]
+    public class PrivacyController : Controller
+    {
+        private readonly ILogger<PrivacyController> _logger;
+        private readonly IUnitOfWork _iUnitOfWork;
+
+        public PrivacyController(ILogger<PrivacyController> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _iUnitOfWork = unitOfWork;
+        }
+        public IActionResult PrivacyPolicy()
+        {
+            return View();
+        }
+    }
+}
